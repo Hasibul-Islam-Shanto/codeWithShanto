7098fffa6df99a8b2afb590bb01809f1fb0b4813
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2025-02-02 19:27:16 +0600

    fix: change the footer name

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from '@/components/footer';
 import MainNav from '@/components/header/main-nav';
+import Footer from '@/components/site-footer';
 const navLinks = [
   {
     title: 'Features',
